d333445f504a71a98d0889371c50383cd1bfd000
Timothy Hoang <timh1203@gmail.com>
2017-10-26 20:54:46 -0400

    Add channel status logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,38 +3,56 @@ window.onload = function() {
     const online = document.getElementById("online-channels");
     const offline = document.getElementById("offline-channels");
     const channels = document.getElementById("channels");
-    const channelArr = ["esl_sc2"];
+    // const channelArr = ["esl_sc2", "freecodecamp", "twitchpresents"];
+    const channel = "twitchpresents";
 
     // Render Channels
     function addChannels(data) {
         const name = data.display_name;
         const game = data.game;
         const logo = data.logo;
-        const status = data.status;
+        const topic = data.status;
         const url = data.url;
         const views = data.views;
         let content = '';
 
-        for (let i = 0; i < channelArr.length; i++) {
-            content += `<a href=${url} target="_blank">
-                <div id="${name}-channel" class="channels">
-                    <h1 id="${name}-name">${name}</h1>
-                    <img src="${logo}"></img>
-                    <h2 id="${game}-game">${game}</h2>
-                    <h3 id="${status}">${status}</h3>
-                </div>
-            </a>`;
-        }
-        console.log(logo);
-
+        fetch(`https://wind-bow.glitch.me/twitch-api/streams/${channel}`)
+            .then(res => res.json())
+            .then(renderChannels)
+            .catch(err => requestError(err, "query"));
+        
+        function renderChannels(response) {
+            if (response.stream === null) {
+                content += `<a href=${url} target="_blank">
+                    <div id="${name}-channel" class="offline">
+                        <h1 id="${name}-name">${name}</h1>
+                        <h2 id="${game}-game">${game}</h2>
+                        <img src="${logo}"></img>
+                        <h2 id="live">Offline</h2>
+                        <h3 id="${topic}">No current stream</h3>
+                    </div>
+                </a>`;
+            }
+            else {
+                content += `<a href=${url} target="_blank">
+                    <div id="${name}-channel" class="online">
+                        <h1 id="${name}-name">${name}</h1>
+                        <h2 id="${game}-game">${game}</h2>
+                        <img src="${logo}"></img>
+                        <h2 id="live">Online</h2>
+                        <h3 id="${topic}">${topic}</h3>
+                    </div>
+                </a>`;    
+            }
         channels.insertAdjacentHTML('beforeend', content);
+        }
     }
     
     // Fetch channels 
-    fetch("https://wind-bow.glitch.me/twitch-api/channels/esl_sc2")
-    .then(res => res.json())
-    .then(addChannels)
-    .catch(err => requestError(err, "query"));
+    fetch(`https://wind-bow.glitch.me/twitch-api/channels/${channel}`)
+        .then(res => res.json())
+        .then(addChannels)
+        .catch(err => requestError(err, "query"));
 
     // Error function
     function requestError(part) {
